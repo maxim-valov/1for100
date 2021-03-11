754badc0c96ab851f86280a3e9423bcb7e2f410b
Maxim Valov <max-valov@yandex.ru>
2021-03-11 13:09:14 +0300

    + readme

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,7 @@ public class Main {
         int account = 100;
         int transfer = 1000;
         int bid = 1;
-        int bonus;
-        if (transfer >= 1000) {
-            bonus = transfer / 100 * bid;
-        } else {
-            bonus = 0;
-        }
+        int bonus = (transfer >= 1000) ? transfer / 100 * bid : 0;
         int balance = account + transfer + bonus;
         System.out.println("Balance: " + balance);
         System.out.println("Bonus: " + bonus);
